78eaf2cf8559ea93ef39686b7c715c9551db6cef
madzida <mv52444@fer.hr>
2021-12-12 10:19:13 +0100

    updated deleteStudent file

--- frontend/src/components/Students/DeleteStudent.js
+++ frontend/src/components/Students/DeleteStudent.js
@@ -1,11 +1,18 @@
 import React,{useState,useEffect} from 'react';
 import {Link} from 'react-router-dom';
 const DeleteStudent =()=>{
-  const [values,setValues]=useState({idstudent:""})
+  const [values,setValues]=useState({
+    ime:"",
+    prezime:"",
+    slicica:"",
+    idRazred:1,
+  });
+  let list=[];
+  const [students,setStudents]=useState({list:[]});
   //ovdje nisam jos nista jer moram vidit kakv ce bit back da znm napravit
   useEffect(async()=>{
     await(fetch('/student/remove', {
-      method: 'POST',
+      method: 'GET',
       headers:{
         'Content-Type':'application/json'
       },
@@ -13,10 +20,32 @@ const DeleteStudent =()=>{
       body: JSON.stringify({values})
     })).then(function(response) {
       return response.json();
+    }).then(data=>{
+      try{
+        console.log(data)
+       for(var i=0;i<data.students.length;i++){
+        list[i]=data.students[i].name;
+        setStations({list:list});
+       }}
+       catch{
+         console.log(data)
+       }
     });
   },[])
+  const handleChange=(e)=>{
+    setValues({
+      ...values,
+      [e.target.name]:e.target.value,});
+  };
 
-  return("shgS");
+  return( <select id="dropdown" 
+  className="input2" 
+  type="text" 
+  name="delete" 
+  value={values}
+  onChange={handleChange}>
+   {students.list.map((item,i) => <option key={i}>{item}</option>)}
+  </select>);
 
 
 };
